20ccac1b94fa53abcf8d9d88e481bbbf076fd557
mtnst <metindir@gmail.com>
2017-11-01 19:37:05 +0300

    edited

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,4 @@ let messages = require('./messages');
 
 let app = document.getElementById('app');
 app.innerHTML = '<p>' + messages.hi + ' ' + messages.event + '</p>'
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const paths = {
     DIST: path.resolve(__dirname, 'dist'),
-    JS: path.resolve(__dirname, 'src'),
+    DIST_JS: path.resolve(__dirname, 'dist/js'),
+    JS: path.resolve(__dirname, 'src/js'),
+    SRC: path.resolve(__dirname, 'src'),
   };
 
 
@@ -12,5 +16,22 @@ module.exports = {
   output: {
     path: paths.DIST,
     filename: 'bundle.js'
-  }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(paths.SRC, 'index.html'),
+    }),
+  ],
+
+  module: {
+    rules: [
+
+
+    ],
+
+
+  },
+
+  
 };
